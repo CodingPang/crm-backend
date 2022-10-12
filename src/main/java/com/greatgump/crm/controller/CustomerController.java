391b6124ff71d810e8afbf043bffc7006253ffbc
Yang <764910696@qq.com>
2022-10-12 19:20:23 +0800

    添加swagger接口文档注释

--- src/main/java/com/greatgump/crm/controller/CustomerController.java
+++ src/main/java/com/greatgump/crm/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.greatgump.crm.controller;
 import com.greatgump.crm.common.R;
 import com.greatgump.crm.entity.Customer;
 import com.greatgump.crm.service.CustomerService;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,18 +20,21 @@ import java.util.List;
  * @author team6
  * @since 2022-10-12 10:31:27
  */
+@Api(tags = "客户模块说明")
 @RestController
 @RequestMapping("/crm")
 public class CustomerController {
     @Autowired
     private CustomerService  customerService;
 
+    @ApiOperation("获取所有客户")
     @GetMapping("/customer")
     public R getAllCustomer(){
         List<Customer> customers = customerService.queryAllCustomer();
 
         return R.ok().put("coutomers",customers);
     }
+    @ApiOperation("获取所有海员")
     @GetMapping("/seas")
     public R getAllSeas(){
         List<Customer> customers = customerService.queryAllSeas();
