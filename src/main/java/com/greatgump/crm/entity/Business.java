dcae0bc7c051e80cb473016e5c5e4c46c5e48bc5
codingpang <2292038494@qq.com>
2022-10-13 20:12:50 +0800

    1、修改Business关联客户属性的类型 2、修改BusinessMapper对应的映射值

--- src/main/java/com/greatgump/crm/entity/Business.java
+++ src/main/java/com/greatgump/crm/entity/Business.java
@@ -3,13 +3,16 @@ package com.greatgump.crm.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableName;
-import java.io.Serializable;
-import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.Getter;
-import lombok.Setter;
+import java.io.Serializable;
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 /**
@@ -18,52 +21,53 @@ import lombok.experimental.Accessors;
  * </p>
  *
  * @author team6
- * @since 2022-10-12 10:31:27
+ * @since 2022-10-13 08:07:56
  */
-@Getter
-@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 @Accessors(chain = true)
 @TableName("t_business")
 @ApiModel(value = "Business对象", description = "商机管理")
 public class Business implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
+  @ApiModelProperty("自增")
+  @TableId(value = "id", type = IdType.AUTO)
+  private Long id;
 
-    @ApiModelProperty("自增")
-    @TableId(value = "id", type = IdType.AUTO)
-    private Long id;
+  @ApiModelProperty("关联客户(外键)")
+  @TableField("customer_id")
+  private Long customerId;
 
-    @ApiModelProperty("用户名")
-    @TableField("username")
-    private String username;
+  @ApiModelProperty("商机标题")
+  @TableField("opportunity_title")
+  private String opportunityTitle;
 
-    @ApiModelProperty("商机标题")
-    @TableField("opportunity_title")
-    private String opportunityTitle;
+  @ApiModelProperty("商机阶段")
+  @TableField("opportunity_stage")
+  private String opportunityStage;
 
-    @ApiModelProperty("商机阶段")
-    @TableField("opportunity_stage")
-    private String opportunityStage;
+  @ApiModelProperty("报价记录")
+  @TableField("price_records")
+  private String priceRecords;
 
-    @ApiModelProperty("报价记录")
-    @TableField("price_records")
-    private String priceRecords;
+  @ApiModelProperty("最后跟进")
+  @TableField("last_follow")
+  @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+  private Date lastFollow;
 
-    @ApiModelProperty("最后跟进")
-    @TableField("last_follow")
-    private Date lastFollow;
+  @ApiModelProperty("未跟进天数")
+  @TableField("not_follow_days")
+  private Integer notFollowDays;
 
-    @ApiModelProperty("未跟进天数")
-    @TableField("not_follow_days")
-    private Integer notFollowDays;
+  @ApiModelProperty("备注")
+  @TableField("remark")
+  private String remark;
 
-    @ApiModelProperty("备注")
-    @TableField("remark")
-    private String remark;
-
-    @ApiModelProperty("假删（0表示未删，1表示删除）")
-    @TableField("is_delete")
-    private Integer isDelete;
-
+  @ApiModelProperty("假删（0表示未删，1表示删除）")
+  @TableField("is_delete")
+  @TableLogic
+  private Integer isDelete;
 
 }
