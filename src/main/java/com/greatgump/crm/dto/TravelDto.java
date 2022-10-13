3eccc0d2473552a6fc8864091d3ec4417bd4cb1b
tao_luo <tao_luo@126.com>
2022-10-13 21:36:21 +0800

    新增出差，借款，请假

---
+++ src/main/java/com/greatgump/crm/dto/TravelDto.java
@@ -0,0 +1,47 @@
+package com.greatgump.crm.dto;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TravelDto {
+
+    @ApiModelProperty("申请人员")
+    @TableField("applicant")
+    private String applicant;
+
+    @ApiModelProperty("出差原因")
+    @TableField("cause")
+    private String cause;
+
+    @ApiModelProperty("开始时间")
+    @TableField("start_time")
+    private Date startTime;
+
+    @ApiModelProperty("结束时间")
+    @TableField("end_time")
+    private Date endTime;
+
+    @ApiModelProperty("出差天数")
+    @TableField("travel_days")
+    private Integer travelDays;
+
+    @ApiModelProperty("审批状态（1表示已通过，2表示待审批，0表示已驳回)")
+    @TableField("approval_status")
+    private String approvalStatus;
+
+    @ApiModelProperty("备注")
+    @TableField("remark")
+    private String remark;
+
+    @ApiModelProperty("假删")
+    @TableField("is_delete")
+    private Integer isDelete;
+}
